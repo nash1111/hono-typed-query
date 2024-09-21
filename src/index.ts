86ec4569bf87dd62fd0fb357f3ab15c306893088
nash1111 <nash1111@users.noreply.github.com>
2024-09-21 22:23:26 +0900

    chore: add comment, vsc settings

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { components } from './types';
 import { PrismaClient } from '@prisma/client';
+import { Console, Effect } from 'effect'
 import * as S from "@effect/schema/Schema";
 
 const prisma = new PrismaClient();
 
 type User = components['schemas']['User'];
+
+
+// TODO: 
+// automatically generate schema ?
+// type User = {
+//   id?: number;
+//   name?: string;
+//   email?: string;
+// }
+const UserSchema = S.Struct({
+  id: S.optional(S.String),
+  name: S.optional(S.String),
+  email: S.optional(S.String),
+});
 
 const app = new Hono()
 
